6836520229140ec78b2de407a0847a4743fb4b1c
Zeljko Majkic <zeljko.majkic@telekom.de>
2021-04-20 22:10:51 +0200

    upload

---
+++ Uebung05/src/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+
+	private String vorname;
+	private String nachname;
+
+	setVorname(String vorname)
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Programm start");
+
+		Person Maja = new Person();
+		maja.setVorname("Maja");
+		
+		System.out.println("Programm ende");
+
+	}
+}
