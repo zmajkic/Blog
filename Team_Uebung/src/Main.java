c9133371782969817448aa3a68f4f06e8af99aab
a298557 <a298557@sid1b572953.lan>
2021-04-23 16:01:12 +0200

    „Kommentar“

---
+++ Team_Uebung/src/Main.java
@@ -0,0 +1,78 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MyList sea = new VerwaltungsGruppe2();
+		//VerwaltungsGruppe1 sea = new VerwaltungsGruppe1();
+	    //VerwaltungsGruppe2 sea = new VerwaltungsGruppe2();
+		
+		Object thomas = new Object();
+		thomas.setVorname("Thomas");
+		thomas.setNachname("Horchem");
+
+		Object agata = new Object();
+		agata.setVorname("Agata");
+		agata.setNachname("Rubin-Jung");
+
+		Object zeljko = new Object();
+		zeljko.setVorname("Zeljko");
+		zeljko.setNachname("Majkic");
+
+		Object thorsten = new Object();
+		thorsten.setVorname("Thorsten");
+		thorsten.setNachname("Lorenz");
+
+		Object o = new Object();
+		o = null;
+		
+		sea.add(o);
+		sea.add(thomas);
+		sea.add(agata);
+		sea.add(zeljko);
+		sea.add(thorsten);
+
+		System.out.println(sea.size());
+		System.out.println("Ausgabe nach anmelden");
+		for (int i = 0; i < 4; i++) {
+			if (sea.get(i) == null) {
+				System.out.println(  i + " is null ");
+			} else {
+			//	System.out.println(sea.getTeilnehmer(i));
+				System.out.println(((Object)sea.get(i)).getVorname());
+			}
+		}
+		
+		
+		
+		
+		System.out.println("Anzahl: " + sea.size());
+		
+		sea.size();
+	//	var teilnehmer = sea.getTeilnehmer(3);
+	//	System.out.println(teilnehmer.getVorname() + " " + teilnehmer.getNachname());
+
+	//	sea.remove(thomas);
+	//	sea.remove(agata);
+	//	sea.remove(null);
+	//	sea.remove(thorsten);
+		
+		
+		System.out.println("Ausgabe nach abmelden" + " Anzahl: " + sea.size());
+
+		
+		for (int i = 0; i < 4; i++) {
+			if (sea.get(i) == null) {
+				System.out.println( i + " is null");
+			} else {
+			//	System.out.println(sea.getTeilnehmer(i));
+				System.out.println(((Object)sea.get(i)).getVorname());
+			}
+		}
+	
+
+		
+	}
+
+}
---
+++ Team_Uebung/src/MyList.java
@@ -0,0 +1,13 @@
+public interface MyList {
+	
+	public boolean add(Object object);
+	
+	public int size();
+	
+	public Object get(int i);
+	
+	public boolean remove(Object object);
+	
+	
+	
+}
