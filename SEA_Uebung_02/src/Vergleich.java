bf52ef1bb6a529c03203cd6d60db97f1f56aac0a
a298557 <a298557@sid1b572953.lan>
2021-04-23 16:15:51 +0200

    qqsq

---
+++ SEA_Uebung_02/src/Vergleich.java
@@ -0,0 +1,15 @@
+
+public class Vergleich {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Zeichen meinersteszeichen; // Deklaration der Referenz
+		meinersteszeichen = new Zeichen(); // Initalisieren der Instanz
+
+	
+		
+		System.out.println(meinersteszeichen);
+	}
+
+}
---
+++ SEA_Uebung_02/src/Zeichen.java
@@ -0,0 +1,14 @@
+
+public class Zeichen { // Klasse
+
+	private char zeichen; // Variable
+
+	public Zeichen() {
+	}
+
+	public void setzeichen(char einwert) { // Methode: "setzeichen"
+		this.zeichen = einwert; // set variable einwert = Referenzvariable
+
+	}
+
+}
